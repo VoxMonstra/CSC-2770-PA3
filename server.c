b91a56b25415d85906d473a169db5b30f6856b48
noahn <ncgregory43@tntech.edu>
2024-11-15 22:40:21 -0600

    server.c

--- server.c
+++ server.c
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/socket.h>
 #include <arpa/inet.h>
 
 #define PORT 8080
@@ -65,15 +64,70 @@ void bind_server_socket(int server_fd, struct sockaddr_in *address) {
 int accept_client_connection(int server_fd, struct sockaddr_in *address) {
     // TODO: Implement accepting client connection
     // You this is where you keep state related to the client. This might be useful for retransmission.
+    char buffer[MAX_PACKET_SIZE];
+    socklen_t client_len = sizeof(*client_addr);
+
+    int received_bytes = recvfrom(server_fd, buffer, MAX_PACKET_SIZE, 0, (struct sockaddr *)client_addr, &client_len);
+
+    if (received_bytes < 0) {
+        perror("Failed to receive initial data from client");
+        return -1;  // Return an error code
+    }
+
+    buffer[received_bytes] = '\0';  // Null-terminate the message
+    printf("Initial message from client: %s\n", buffer);
+
+    // Optionally send acknowledgment or process initial message
+    const char *ack = "ACK_INIT";
+    if (sendto(server_fd, ack, strlen(ack), 0, (struct sockaddr *)client_addr,
+               client_len) < 0) {
+        perror("Failed to send acknowledgment");
+    }
     return 0;
 }
 
 // Function to handle communication with the client
 void handle_client(int client_socket) {
     // TODO: Implement the logic to receive and send data to the client
+    char buffer[MAX_PACKET_SIZE + 1];  // Buffer to store received data
+    socklen_t client_len = sizeof(*client_addr);
+    int received_bytes;
+
+    // Static buffers for message reassembly
+    static char message[1024];
+    static int message_offset = 0;
+
+    // Receive data from the client
+    received_bytes = recvfrom(server_fd, buffer, MAX_PACKET_SIZE, 0,
+                              (struct sockaddr *)client_addr, &client_len);
+    if (received_bytes < 0) {
+        perror("Failed to receive data");
+        return;
+    }
+
+    buffer[received_bytes] = '\0';  // Null-terminate the received string
+    printf("Received packet: %s\n", buffer);
+
+    // Handle reassembly
+    if (strcmp(buffer, "END") == 0) {
+        printf("Complete message received: %s\n", message);
+        memset(message, 0, sizeof(message));
+        message_offset = 0;
+    } else {
+        memcpy(message + message_offset, buffer, received_bytes);
+        message_offset += received_bytes;
+    }
+
+    // Send acknowledgment to the client
+    const char *ack = "ACK";
+    if (sendto(server_fd, ack, strlen(ack), 0, (struct sockaddr *)client_addr, client_len) < 0) {
+        perror("Failed to send acknowledgment");
+    }
 }
 
 // Function to close the server socket
 void close_server_socket(int server_fd) {
     // TODO: Implement closing the server socket
+    close(server_fd);
+    printf("Server socket closed\n");
 }
